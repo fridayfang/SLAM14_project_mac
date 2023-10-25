2bd83a1bc7eacc24cce82f62b85b731cd410222c
fridayfang <fang_shuai@pku.edu.cn>
2023-10-25 11:12:21 +0800

    test gpt cr action

--- test/test_triangulation.cpp
+++ test/test_triangulation.cpp
@@ -28,5 +28,6 @@ TEST(MyslamTest, Triangulation) {
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    std::cout << "test for cr action\n";
     return RUN_ALL_TESTS();
 }
